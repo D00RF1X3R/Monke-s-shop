115a61d46a5f9d731e4f81ea404c31b6aefcb340
ttoddo <hramchencov@gmail.com>
2024-06-24 18:42:21 +0300

    profile pages

--- static_dev/js/scripts.js
+++ static_dev/js/scripts.js
@@ -1,4 +1,10 @@
-function favoriteColor(color){
-    document.getElementsByClassName("main_products_card_favorite__image").style.fill = "red";
-    return false
+function favoriteColor(button){
+    var svg = document.getElementsByClassName("main_products_card_favorite__image")[0];
+    console.log(svg);
+    if (document.svg == "black"){
+        svg.fill  = "red";
+    } else {
+        svg.fill = "black";
+    }
+    return false;
 }
