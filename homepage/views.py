a6d765ebcdee630ee1ded1d55bf3f0286d4d518a
dantes <shmychkov210605@gmail.com>
2024-06-28 17:40:05 +0300

    Fix bugs

--- homepage/views.py
+++ homepage/views.py
@@ -8,13 +8,17 @@ class HomeView(ProductListView):
     template_name = "homepage/geekShop.html"
     def get_context_data(self):
         context =  super().get_context_data()
-        customer_data = get_object_or_404(CustomerData, user=self.request.user)
-        favorite_categories = Product.objects.filter(category__id__in=customer_data.favorite_categories.all(),
-                                                     universe__id__in=customer_data.favorite_universes.all())
-        bad_products = Product.objects.exclude(category__id__in=customer_data.favorite_categories.all(),
-                                               universe__id__in=customer_data.favorite_universes.all())
-        favorite_products = list(chain(favorite_categories, bad_products))
-        context["products"] = favorite_products
+        try:
+            customer_data = CustomerData.objects.get(user=self.request.user.id)
+        except CustomerData.DoesNotExist:
+            customer_data = None
+        if customer_data:
+            favorite_categories = Product.objects.filter(category__id__in=customer_data.favorite_categories.all(),
+                                                        universe__id__in=customer_data.favorite_universes.all())
+            bad_products = Product.objects.exclude(category__id__in=customer_data.favorite_categories.all(),
+                                                universe__id__in=customer_data.favorite_universes.all())
+            favorite_products = list(chain(favorite_categories, bad_products))
+            context["products"] = favorite_products
         return context
 
 
